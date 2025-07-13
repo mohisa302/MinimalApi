d15e3d45b853a2528e340975fb890ddc131b0576
mohisa302 <mohisadeghi302@gmail.com>
2025-07-13 17:54:04 +0330

    Add part 1

---
+++ UserDB/dbo/StoredProcedures/spUser_Insert.sql
@@ -0,0 +1,10 @@
+﻿CREATE PROCEDURE [dbo].[spUser_Update]
+	@Id int,
+	@FirstName NVARCHAR(50),
+	@LastName NVARCHAR(50)
+AS
+begin
+	update dbo.[User] 
+	set FirstName = @FirstName, LastName = @LastName
+	where Id = @Id;
+end
---
+++ UserDB/dbo/StoredProcedures/spUser_Update.sql
@@ -0,0 +1,8 @@
+﻿CREATE PROCEDURE [dbo].[spUser_Insert]
+	@FirstName NVARCHAR(50),
+	@LastName NVARCHAR(50)
+AS
+begin
+	insert into dbo.[User] (FirstName, LastName)
+	values (@FirstName, @LastName);
+end
